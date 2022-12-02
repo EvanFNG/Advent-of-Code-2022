61a55c9f8af2dfa0dbe33b56e207b888ac886321
esmith <esmith@truecar.com>
2022-12-02 12:59:34 -0600

    Day 2 solved

--- day2.py
+++ day2.py
@@ -47,11 +47,58 @@ def rock_paper_scissors(move_list: list) -> tuple:
 
         return round_result_dict[translation]
 
+    # Handles case when the opposite order of an existing key is passed in,
+    # e.g. ('Paper', 'Rock') instead of ('Rock', 'Paper')
     else:
         return round_result_dict[translation[::-1]][::-1]
 
-scores = list(map(rock_paper_scissors, data))
+scores = [rock_paper_scissors(i) for i in data]
 
+# Sum of this is the asnwer to part 1
 p2_scores = [i[1] for i in scores]
 
-print(sum(p2_scores))
+def p2_score(p1_move: str, desired_outcome: str) -> int:
+    '''
+    Determines the correct response from Player 2 according to Player 1's move
+    and the code in column 2 of data.
+    Returns player 2's score.
+    '''
+
+    # A: Rock, B: Paper, C: Scissors
+    shape_values = {'A': 1, 'B': 2, 'C': 3}
+
+    # X: Lose, Y: Draw, Z: Win
+    outcome_values = {'X': 0, 'Y': 3, 'Z': 6}
+
+    match desired_outcome:
+
+        # Draw
+        case 'Y':
+
+            return outcome_values[desired_outcome] + shape_values[p1_move]
+
+        # Win
+        case 'Z':
+
+            if p1_move == 'A':
+                return outcome_values[desired_outcome] + shape_values['B']
+
+            elif p1_move == 'B':
+                return outcome_values[desired_outcome] + shape_values['C']
+
+            else:
+                return outcome_values[desired_outcome] + shape_values['A']
+
+        # Lose
+        case 'X':
+
+            if p1_move == 'A':
+                return outcome_values[desired_outcome] + shape_values['C']
+
+            elif p1_move == 'B':
+                return outcome_values[desired_outcome] + shape_values['A']
+
+            else:
+                return outcome_values[desired_outcome] + shape_values['B']
+
+print(sum([p2_score(i[0], i[1]) for i in data]))
