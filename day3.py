4ead97098f32637e420a23e72670d5085a16f1ce
EvanFNG <85416292+EvanFNG@users.noreply.github.com>
2022-12-03 09:13:09 -0600

    Day 3 part 2 solved

--- day3.py
+++ day3.py
@@ -1,4 +1,6 @@
 from string import ascii_lowercase, ascii_uppercase
+from functools import reduce
+from itertools import zip_longest
 
 def split_half(s: str) -> list:
     '''
@@ -10,13 +12,34 @@ def split_half(s: str) -> list:
 
     return [s[:x], s[x:]]
 
-def list_shared_elements(l1: list, l2: list) -> list:
+def list_intersect(*args) -> list:
     '''
     Returns a list of elements that are shared
-    between the two input lists.
+    between the input lists.
     '''
+    if len(args) == 1:
+        intersect = reduce(lambda x, y: set(x) & set(y), args[0])
+    else:
+        intersect = reduce(lambda x, y: set(x) & set(y), args)
 
-    return list(set(l1) & set(l2))
+    return list(intersect)
+
+# From the itertools recipe
+# https://docs.python.org/3/library/itertools.html
+def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
+    "Collect data into non-overlapping fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
+    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
+    # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
+    args = [iter(iterable)] * n
+    if incomplete == 'fill':
+        return zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == 'strict':
+        return zip(*args, strict=True)
+    if incomplete == 'ignore':
+        return zip(*args)
+    else:
+        raise ValueError('Expected fill, strict, or ignore')
 
 lower_dict = {letter: value + 1 for value, letter in enumerate(ascii_lowercase)}
 upper_dict = {letter: value + 27 for value, letter in enumerate(ascii_uppercase)}
@@ -29,8 +52,14 @@ with open('input_files/day3.txt', 'r') as f:
 
 split_inputs = [split_half(i) for i in data]
 
-common_items = [list_shared_elements(i[0], i[1]) for i in split_inputs]
+common_items = [list_intersect(i[0], i[1]) for i in split_inputs]
 
+# Sum of this is solution to part 1
 item_values = [letter_dict[i[0]] for i in common_items]
 
-print(sum(item_values))
+groups = list(grouper(iterable = data, n = 3))
+
+group_common_items = [list_intersect(i) for i in groups]
+
+# Part 2 solution
+print(sum([letter_dict[i[0]] for i in group_common_items]))
