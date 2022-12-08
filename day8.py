5e8c2c387c5db4b4af6d6cc7b8466ab5f6648d9a
esmith <esmith@truecar.com>
2022-12-08 15:45:21 -0600

    Making grid functions

--- day8.py
+++ day8.py
@@ -10,15 +10,50 @@ for i in data:
 
 print('\n')
 
-for row_index, row in enumerate(data):
-    # Determine the three arrays of trees to check
-    if row_index == 0:
-        tree_lists = [row, data[row_index+1], data[row_index+2]]
-    elif row_index == len(data):
-        tree_lists = [data[-3], data[-2], row]
-    else:
-        tree_lists = [data[row_index-1], row, data]
+def neighbor_coords(start: list[int]) -> list[list[int]]:
+    '''
+    Returns the coordinates of the neighbors of start.
+
+    >>> neighbor_coords([1, 3])
+    [[2, 3], [0, 3], [1, 2], [1, 4]]
+    '''
+    
+    x, y = start
+    return [[x+1, y], [x-1, y], [x, y-1], [x, y+1]]
+
+def tree_neighbors(point_index: list[int], arr: list[list[int]]) -> list[int]:
+    '''
+    Returns a tree's neighbors given its index
+    and the 2D array it's contained in.
+
+    >>> tree_neighbors([1, 1], [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    [2, 4, 6, 8]
+    '''
+
+    coords = neighbor_coords(point_index)
+    neighbors = []
+
+    
+
+
+# def count_visible_trees(arr: list[list[int], list[int], list[int]]) -> int:
+#     init_count = 0
+
+#     for rows in arr[:2]:
+#         pass
 
 
 
-print(viz_count)
+
+# for row_index, row in enumerate(data):
+#     # Determine the three arrays of trees to check
+#     if row_index == 0:
+#         tree_lists = [row, data[row_index+1], data[row_index+2]]
+#     elif row_index == len(data):
+#         tree_lists = [data[-3], data[-2], row]
+#     else:
+#         tree_lists = [data[row_index-1], row, data]
+
+
+
+print(neighbor_coords([0,0]))
