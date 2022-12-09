4a9e9b090f2bbbfd749ea029b61eba47db078d6c
esmith <esmith@truecar.com>
2022-12-09 17:12:41 -0600

    Renamed the renamed the arr __init__ param to forest

--- day8.py
+++ day8.py
@@ -4,16 +4,16 @@ with open('input_files/day8.txt', 'r') as f:
 
 class Tree:
 
-    def __init__(self, coords: list[int], arr: data = list[list[int]]) -> None:
+    def __init__(self, coords: list[int], forest: list[list[int]]) -> None:
         self.coords = coords
-        self.arr = arr
+        self.forest = forest
 
         # Assume square array
-        self.max_index = len(arr[0]) - 1
+        self.max_index = len(forest[0]) - 1
 
     def height(self) -> int:
         x, y = self.coords
-        return self.arr[x][y]
+        return self.forest[x][y]
 
     def location(self) -> str:
         '''
@@ -29,17 +29,17 @@ class Tree:
     def neighbors_all(self) -> list[list[int]]:
 
         x, y = self.coords
-        l = [[i for i in self.arr[x][:y]], [j for j in self.arr[x][y+1:]]]
+        l = [[i for i in self.forest[x][:y]], [j for j in self.forest[x][y+1:]]]
 
         col1 = []
         col2 = []
 
-        for row in self.arr[:x]:
+        for row in self.forest[:x]:
                 col1.append(row[y])
 
         l.append(col1)
 
-        for row in self.arr[x+1:]:
+        for row in self.forest[x+1:]:
             col2.append(row[y])
         
         l.append(col2)
@@ -93,7 +93,7 @@ scores = []
 for row_index, row in enumerate(data):
     for tree_index, tree in enumerate(row):
 
-        curr_tree = Tree(coords = [row_index, tree_index], arr = data)
+        curr_tree = Tree(coords = [row_index, tree_index], forest = data)
 
         # Part One
         neighbors = curr_tree.neighbors_all()
