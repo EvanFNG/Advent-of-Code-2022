28e8a20a5e2b99baca8bcce215da9fd8b5098818
esmith <esmith@truecar.com>
2022-12-09 14:17:02 -0600

    Redid part 1 using Tree class

--- day8.py
+++ day8.py
@@ -243,63 +243,24 @@ class Tree:
                 return 0
 
 # Running total of visible trees
-viz_count = 0
-
-def tree_neighbors_all(point: list[int], arr: list[list[int]]) -> list[list[int]]:
-    '''
-    Similar to tree_neighbors_adjacent,
-    but returns the entire neighboring column and row.
-    Always returns in order [left, right, up, down]
-
-    >>> tree_neighbors_all([1, 1], [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    [[4], [6], [2], [8]]
-    '''
-
-    x, y = point
-
-    l = [[i for i in arr[x][:y]], [j for j in arr[x][y+1:]]]
-
-    col1 = []
-    col2 = []
-
-    for row in arr[:x]:
-            col1.append(row[y])
-
-    l.append(col1)
-
-    for row in arr[x+1:]:
-        col2.append(row[y])
-    
-    l.append(col2)
-
-    l = [i for i in l if i != []]
-
-    return l
-
-data_length = len(data)
-
-# viz_count will be the answer to part 1
-for row_index, row in enumerate(data):
-    for tree_index, tree in enumerate(row):
-        x, y = [row_index, tree_index]
-        neighbors = tree_neighbors_all([x, y], data)
-        neighbors_max = [max(i) for i in neighbors]
-        viz_bool = any([tree > i for i in neighbors_max])
-
-        if any(
-            [
-                viz_bool,
-                row_index in [0, data_length - 1],
-                tree_index in [0, data_length - 1]
-            ]
-        ):
-            viz_count += 1
+visibile_count = 0
 
 scores = []
 
 for row_index, row in enumerate(data):
     for tree_index, tree in enumerate(row):
+
         curr_tree = Tree(coords = [row_index, tree_index], arr = data)
+
+        # Part One
+        neighbors = curr_tree.neighbors_all()
+        neighbors_max = [max(i) for i in neighbors]
+        visible = any([curr_tree.height() > i for i in neighbors_max])
+
+        if visible or (curr_tree.location() != 'Inner'):
+            visibile_count += 1
+
+        # Part Two
         scores.append(curr_tree.scenic_score())
 
 part_two_solution = max(scores)
